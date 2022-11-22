f3c392c0f690d392b3e85ad08c1709aaf6ea0785
Gitarackur <peterosah5@gmail.com>
2022-11-22 07:43:47 +0100

    code for building a blog with butterCMS and nuxt

---
+++ pages/[id].vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="md:w-6/12 mx-auto">
+      <div class="flex items-center justify-between py-10 md:py-6">
+        <div class='py-10 md:py-20'>
+          <div>
+            <a class='cursor-pointer' @click="()=> router.go(-1)"> BACK </a>
+          </div>
+        </div>
+
+        <div>
+          <NuxtLink to="/" class='heading-text'>ButterCMS Blog</NuxtLink>
+        </div>
+      </div>
+
+
+
+      <div>
+        <div>
+          <div class='relative inline-block w-full'>
+            <img :src="post?.featured_image" class='blog-thumbnail h-20' />
+            <p class='mt-8' style="color: #ccc">
+              created at: {{ new Date(post?.created).toLocaleString('en-US')}}
+            </p>
+            <div class='mt-20'>
+              <h1 class='heading-text'>
+                {{ post?.title }}
+              </h1>
+              <div class='mt-8'>
+                <div v-html="post?.body"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const router = useRouter()
+  const route = useRoute()
+
+  const { $api } = useNuxtApp()
+
+  const post = ref<any>(null)
+  
+  onMounted(async () => {
+    try {
+      const res = await $api.getPost(route.params.id);
+      post.value = res.data;
+      console.log('response', res);
+    } catch(err: any) {
+      throw new Error(`api error for single post--- ${err?.response?.data}`)
+    } finally {
+      console.log('------working-------');
+    }
+  });
+</script>
+
+<style>
+
+</style>
